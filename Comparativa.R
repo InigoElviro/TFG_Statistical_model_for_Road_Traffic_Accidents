92aa9bc62abfad21bf55a236253bd65002bc579e
Íñigo Elviro Muñoz <113517877+InigoElviro@users.noreply.github.com>
2022-09-16 10:09:00 +0200

    Update Comparativa.R

--- Comparativa.R
+++ Comparativa.R
@@ -1,30 +1,6 @@
 #Comparativa#
 #------------------------------------#
 
-comp_plaza=c(104,63,24)
-comp_cuarte=c(69,34,27)
-m_comp = (rbind(comp_plaza, comp_cuarte))
-rownames(m_comp) <- c("Plaza", "Cuarte")
-colnames(m_comp) <- c("09-16", "17-19", "20-21")
-ylim.sup=max(m_comp)*1.2
-aux.ylim=c(0,ylim.sup)
-bp=barplot(m_comp, beside=TRUE,legend = TRUE, ylim=aux.ylim, args.legend = list(bty = "n", x = "topright"), cex=1, cex.main=1.25, cex.names = 1, col = c("steelblue","brown2"))
-text(bp, (as.matrix(m_comp)), round((as.matrix(m_comp)), 2),cex=1,pos=3,cex.axis=1)
-
-
-comp_plaza=c(17,16,12)
-comp_cuarte=c(11,9,13)
-m_comp = (rbind(comp_plaza, comp_cuarte))
-rownames(m_comp) <- c("Plaza", "Cuarte")
-colnames(m_comp) <- c("09-16", "17-19", "20-21")
-ylim.sup=max(m_comp)*1.2
-aux.ylim=c(0,ylim.sup)
-bp=barplot(m_comp, beside=TRUE,legend = TRUE, ylim=aux.ylim, args.legend = list(bty = "n", x = "topright", cex=2), cex=1,cex.axis=2, cex.main=2.5, cex.names = 2, col = c("steelblue","brown2"))
-text(bp, (as.matrix(m_comp)), labels=(paste0(as.matrix(m_comp), '%')),cex=2,pos=3)
-#dev.print(pdf, 'comparativa_01.pdf' ,  height=10, width=10 )
-
-
-
 aux.09=informe(delta_poligonos,c(2009:2016))
 aux.17=informe(delta_poligonos,c(2017:2019))
 aux.20=informe(delta_poligonos,c(2020:2021))
